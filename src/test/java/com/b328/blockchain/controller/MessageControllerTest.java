846a25f40d0583d4e819280b2459421639ee14f1
CastJo <fjchfjch@163.com>
2019-06-15 15:23:54 +0800

    test

---
+++ src/test/java/com/b328/blockchain/controller/MessageControllerTest.java
@@ -0,0 +1,34 @@
+package com.b328.blockchain.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getMessagePage() {
+    }
+
+    @Test
+    public void getMessageById() {
+    }
+
+    @Test
+    public void addMessage() {
+    }
+
+    @Test
+    public void sign() {
+    }
+}
